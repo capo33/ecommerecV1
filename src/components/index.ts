0429c3fd778f078990f27df23924ac3c7019d33a
Mohamed <s2000588@edu.bc.fi>
2023-10-09 21:55:53 +0300

    add FeatureCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header/Header";
 export { default as Hero } from "./Hero/Hero";
- 
+export { default as Products } from "./Products/Products";
+export { default as FeatureCard } from "./FeatureCard/FeatureCard";
--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,10 +1,20 @@
-import { Hero } from "../../components";
+import { FeatureCard, Hero, Products } from "../../components";
 
 const Home = () => {
   return (
     <>
-    <Hero />
-     </>
+      <Hero />
+      <div className='flex flex-col text-center w-full mt-20'>
+        <h2 className='text-xs text-indigo-500 tracking-widest font-medium title-font mb-1'>
+          Products
+        </h2>
+        <h1 className='sm:text-3xl text-2xl font-medium title-font text-gray-900'>
+          Most Popular Products
+        </h1>
+      </div>
+      <Products />
+      <FeatureCard />
+    </>
   );
 };
 
