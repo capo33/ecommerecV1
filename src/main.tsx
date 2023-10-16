020facf948b449c2320f1a1d635c01b8f1327ff6
Mohamed <s2000588@edu.bc.fi>
2023-10-16 16:32:32 +0300

    initiate amplify

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 import routes from "./routes/routes.tsx";
 import { store } from "./redux/app/store.ts";
 
 import "./index.css";
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
