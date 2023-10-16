be697a9e13cde81f667b827265fbe6e0253ded64
Mohamed <s2000588@edu.bc.fi>
2023-10-16 14:33:28 +0300

    add titlteText component to contact component

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,10 +1,11 @@
-import { ContactCard } from "../../components";
+import { ContactCard, TitleText } from "../../components";
 import { Address, Email, Phone } from "../../assets/svg/ContactSVG";
 
 const Contact = () => {
   return (
     <section className='text-gray-600 body-font '>
       <div className='container px-5 py-32 mx-auto'>
+      <TitleText text2='Contact me' />
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
           <ContactCard
             icon={Email}
